042b1ac88f2938d736cc1fb7d899027de5d3893f
Juan Torres <juanin.torres@gmail.com>
2012-10-24 06:06:47 -0300

    Agregando desde Netbeans

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="es_ES"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="es_ES"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="es_ES"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="es_ES"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Simulador Hipotecario</title>
+	<meta name="description" content="Sistema de simulacion de crédito hipotecario">
+	<meta name="author" content="Juan Torres">
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/ui-lightness/jquery-ui-1.9.0.custom.min.css">
+	<script src="js/libs/modernizr-2.5.3-respond-1.1.0.min.js"></script>
+</head>
+<body>
+	<header>
+
+	</header>
+	<div role="main">
+            Hola !!!
+	</div>
+	<footer>
+
+	</footer>
+
+	<script>window.jQuery || document.write('<script src="js/libs/jquery-1.8.2.min.js"><\/script>')</script>
+        <script src="js/libs/jquery-ui-1.9.0.custom.min.js"></script>
+	<script src="js/plugins.js"></script>
+	<script src="js/script.js"></script>
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,4 @@
+/* Author: Juan Torres
+
+*/
+
